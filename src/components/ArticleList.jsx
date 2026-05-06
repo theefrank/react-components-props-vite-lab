dc8d0ae01fa224c120d6c14a060fcb4f65d25325
wxnyeki <frankwanyeki01@gmail.com>
2026-05-06 09:20:23 +0300

    Update ArticleList.jsx

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -1,1 +1,18 @@
+import Article from "./Article";
 
+function ArticleList({ posts }) {
+  return (
+    <main>
+      {posts.map((post, index) => (
+        <Article
+          key={index}
+          title={post.title}
+          date={post.date}
+          preview={post.preview}
+        />
+      ))}
+    </main>
+  );
+}
+
+export default ArticleList;
